bd9f7029e4910857b3855d7429337e921cd4d331
Arthur Gonzales <gnzlsarthur@gmail.com>
2024-03-30 00:51:51 +0100

    Update & Change

--- TP8/Exo1TP8.py
+++ TP8/Exo1TP8.py
@@ -2,5 +2,13 @@
 
 from pnm import *
 voir_fichier('space-invader-pieuvre.pbm' , quadrillage=True)
-voir_fichier('space-invader-soucoupe.pbm')
+#voir_fichier('space-invader-soucoupe.pbm')
+#voir_fichier('space-invader-pieuvre-test.pbm', quadrillage=True)
+
+#M = voir_fichier('space-invader-pieuvre.pbm')
+#print(M)
+
+M = fichier_vers_matrice('space-invader-pieuvre.pbm')
+M[3][4] = 1
+matrice_vers_fichier(M,'space-invader-pieuvre2.pbm')
 voir_fichier('space-invader-pieuvre2.pbm')
